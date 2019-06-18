31e1d20bc2dfc89cfac2d488dde0cbe2d99c3d6e
github username <github email address>
2019-06-18 13:41:11 -0700

    test

--- lib/cli_interface.rb
+++ lib/cli_interface.rb
@@ -1,1 +1,34 @@
 #here we will put all of our messages for user input
+def welcome
+  puts "Hello! Welcome to Github Jobs!"
+end
+
+def user_login
+  puts "Are you a new user or existing user? Please input a number."
+  puts "1. New User"
+  puts "2. Existing User"
+end
+
+def returning_user
+  puts "Welcome back, #{user_nameTBD}! What would you like to do? Please input a number."
+  puts "1. Search for a job"
+  puts "2. See what jobs I have applied for."
+  puts "3. Udpate my profile."
+  puts "4. Delete an application."
+end
+
+def job_search
+  #user will search for job here
+end
+
+def update
+  #user can update their location
+end
+
+def delete
+  #user can delete applications
+end
+
+def user_create
+  #user will input info first
+end
