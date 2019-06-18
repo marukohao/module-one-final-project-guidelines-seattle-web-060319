07c8221f3c7e43f8b9d24e04699706edf1c53d69
github username <github email address>
2019-06-18 14:09:54 -0700

    CLI_interface updated

--- lib/cli_interface.rb
+++ lib/cli_interface.rb
@@ -13,22 +13,41 @@ def returning_user
   puts "Welcome back, #{user_nameTBD}! What would you like to do? Please input a number."
   puts "1. Search for a job"
   puts "2. See what jobs I have applied for."
-  puts "3. Udpate my profile."
+  puts "3. Udpate my information."
   puts "4. Delete an application."
+  puts "5. Exit."
 end
 
-def job_search
+def job_search_descrption(returning_user_response)
   #user will search for job here
+  puts "Great. What are some keywords you are looking for in a job. It could job title, or languages you use."
+end
+
+def job_search_location
+  puts "Great, please enter a location by city, then state. e.g 'Seattle, WA'"
+end
+
+def job_search_FTE
+  puts "Do you only want full-time jobs? Please choose a number."
+  puts "1. Yes."
+  puts "2. No."
 end
 
 def update
-  #user can update their location
+  puts "Where do you live now?"
+  # puts "Do you want to add more languages to your profile?"
 end
 
 def delete
-  #user can delete applications
+  puts "Please choose which application you would like to delete."
 end
 
 def user_create
-  #user will input info first
+  puts "Please enter your full name"
+  username = gets.chomp
+  puts "Please enter your birthday (mm/dd/yyyy)"
+  user_birthday =gets.chomp
+  User.create(name: username, birthday: user_birthday)
 end
+
+def existing_user_info
